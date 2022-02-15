e929505975a1110e9319a22408682f165dd50e51
Sebastian-Lehrig1 <Sebastian.Lehrig1@ibm.com>
2022-02-15 15:16:28 +0100

    Initial installation script version

---
+++ install_kubeflow.sh
@@ -0,0 +1,204 @@
+###########################################################################################################################
+# Kubeflow installation script
+#
+# Author: Sebastian Lehrig
+# License: Apache-2.0 License
+###########################################################################################################################
+
+###########################################################################################################################
+# 1. Prerequisites
+kubeflow_version="v1.4.1"
+
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+echo "${BOLD}Which Kubernetes environment do you have admin access to?${NORMAL}
+(1) Red Hat OpenShift
+(2) Vanilla Kubernetes"
+read -p "Selection [1|2]: " kubernetes_environment
+case "$kubernetes_environment" in 
+  1 ) kubernetes_environment_name="Red Hat OpenShift";;
+  2 ) kubernetes_environment_name="Vanilla Kubernetes";;
+  * ) echo "invalid - exiting"; exit;;
+esac
+
+echo "\n${BOLD}To avoid toomanyrequests errors for Docker.io, do you want to store your Docker.io credentials?${NORMAL}"
+read -p "[y|n]: " store_credentials
+case "$store_credentials" in 
+  y|Y ) while true; do
+          read -p "Docker.io user name: " docker_user
+	  read -s -p "Docker.io password (input hidden): " docker_pass
+	  echo "\nTrying to log-in..." 
+	  logged_in=$(echo $docker_pass | docker login --username ${docker_user} --password-stdin)
+          echo ""
+	  if [[ "${logged_in}" == "Login Succeeded" ]]
+	  then
+            echo "${GREEN}Success${NC}: Docker was able to login to docker.io using your credentials!"  
+	    break
+          fi
+	  echo "${RED}Failed${NC}: Docker was unable to login to docker.io using your credentials! Please verify you have used the correct ones and try again!"
+	done
+        ;;
+  n|N ) ;;
+  * ) echo "invalid - exiting"; exit;;
+esac
+
+echo ""
+read -p "${BOLD}Update your .bashrc file with Kubeflow variables (note: this is required if not already present)?${NORMAL} [y|n]: " update_bashrc
+case "$update_bashrc" in 
+  y|Y ) ;;
+  n|N ) ;;
+  * ) echo "invalid - exiting";;
+esac
+
+echo ""
+echo "${BOLD}====================================================${NORMAL}"
+echo "${BOLD}Installation summary${NORMAL}"
+echo "${BOLD}====================================================${NORMAL}"
+echo "- ${BOLD}Kubeflow${NORMAL}: ${kubeflow_version}"
+echo "- ${BOLD}Kubernetes environment${NORMAL}: ${kubernetes_environment_name}"
+echo "- ${BOLD}Store Docker.io credentials${NORMAL}: ${store_credentials}"
+echo "- ${BOLD}Update .bashrc file${NORMAL}: ${update_bashrc}"
+echo "${BOLD}====================================================${NORMAL}"
+read -p "${BOLD}Proceed Kubeflow installation?${NORMAL} [y|n]: " proceed
+case "$proceed" in
+  y|Y ) ;;
+  n|N ) echo "Kubeflow installation aborted."; exit;;
+  * ) echo "invalid - exiting"; exit;;
+esac
+
+###########################################################################################################################
+# 2. Prepare Installation
+case "$store_credentials" in 
+  y|Y ) case "$kubernetes_environment" in
+        1 ) # Add docker.io account settings into OpenShift secret 
+            oc get secret/pull-secret -n openshift-config --template='{{index .data ".dockerconfigjson" | base64decode}}' > dockerconfig.json
+            oc registry login --registry="docker.io" --auth-basic="$DOCKER_USER:$DOCKER_PW" --to=dockerconfig.json
+            oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson=dockerconfig.json
+            rm -f dockerconfig.json
+	    ;;
+        2 ) # Add docker.io as imagePullSecret to default serviceaccount 
+            kubectl create secret docker-registry myregistrykey --docker-server=docker.io --docker-username=$DOCKER_USER --docker-password=$DOCKER_PW
+            kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "myregistrykey"}]}'
+            ;;
+        esac 	 
+        ;;
+  * ) ;;
+esac
+
+case "$update_bashrc" in
+  y|Y ) cat >> /root/.bashrc <<'EOF'
+# clusterDomain equals oc get ingresses.config/cluster -o jsonpath={.spec.domain}
+export clusterDomain=apps.$(dnsdomainname)
+export externalIpAddress=$(hostname -i)
+export KUBEFLOW_BASE_DIR=/export
+export GIT=$KUBEFLOW_BASE_DIR/git
+export MANIFESTS=$GIT/manifests
+EOF	
+	case "$kubernetes_environment" in
+        1 ) # OpenShift
+            cat >> /root/.bashrc <<'EOF'
+export KUBEFLOW_KUSTOMIZE=$MANIFESTS/overlays/openshift
+export KUBE_PW=$(cat $(find /root -name "kubeadmin-password"))
+oc login -u kubeadmin -p $KUBE_PW --insecure-skip-tls-verify=true
+EOF
+            ;;
+        2 ) # k8s
+	    cat >> /root/.bashrc <<'EOF'
+export KUBEFLOW_KUSTOMIZE=$MANIFESTS/overlays/k8s
+EOF
+            ;;
+        esac
+	source /root/.bashrc
+        ;;
+  * ) ;;
+esac
+
+# Get manifests
+git clone --branch main https://github.com/lehrig/kubeflow-ppc64le-manifests.git $MANIFESTS
+
+###########################################################################################################################
+# 3. Installation
+case "$kubernetes_environment" in
+1 ) # OpenShift
+
+# Install Cert Manager Operator
+# See: https://cert-manager.io/docs/installation/openshift/
+# TODO: Try from OperatorHub (when Kubeflow supports higher cert-manager versions)
+oc new-project cert-manager
+oc apply -f https://github.com/jetstack/cert-manager/releases/download/v1.5.4/cert-manager.yaml
+
+# Install subscriptions (operators from OperatorHub)
+oc apply --kustomize $KUBEFLOW_KUSTOMIZE/subscriptions
+
+# Deploy Kubeflow
+while ! oc kustomize $KUBEFLOW_KUSTOMIZE | oc apply --kustomize $KUBEFLOW_KUSTOMIZE; do echo "Retrying to apply resources"; sleep 10; done
+#############################################
+;;
+2 ) # k8s
+
+# Deploy Kubeflow
+while ! kubectl kustomize $KUBEFLOW_KUSTOMIZE | kubectl apply --kustomize $KUBEFLOW_KUSTOMIZE; do echo "Retrying to apply resources"; sleep 10; done
+;;
+esac
+
+###########################################################################################################################
+# 4. Post-installation cleanup & configuration
+
+# cache-deployer and cache-server not supported yet (are optional anyways)
+kubectl delete deployment cache-deployer-deployment cache-server -n kubeflow
+
+# delete all the kfserving components except UI, because they're not ported to Power yet
+oc delete statefulset kfserving-controller-manager -n kubeflow
+
+case "$kubernetes_environment" in
+1 ) # OpenShift
+
+# Required by visualization server
+# TODO: only 1 of these is required...
+oc adm policy add-scc-to-user privileged -z ml-pipeline-visualizationserver -n kubeflow
+oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:kubeflow:ml-pipeline-visualizationserver
+
+# Required for training operator (regular TFJob & Katib)
+# TODO: Find out which concrete role needs to be set to spawn training pods (--> https://ibm-systems-power.slack.com/archives/CEA8J8WQ6/p1644315444602459)
+oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:kubeflow:training-operator
+
+# HTPasswd & Default User
+# See: https://computingforgeeks.com/manage-openshift-okd-cluster-users-using-htpasswd-identity-provider/
+yum -y install httpd-tools
+htpasswd -c -B -b $KUBEFLOW_BASE_DIR/ocp_users.htpasswd user@example.com 12341234
+oc create secret generic htpass-secret \
+  --from-file=htpasswd=$KUBEFLOW_BASE_DIR/ocp_users.htpasswd \
+  -n openshift-config
+oc apply -f $KUBEFLOW_KUSTOMIZE/servicemesh/htpasswd-oauth.yaml
+
+# Expose minio
+oc expose service -n kubeflow minio-service
+
+# Get UI address
+# TODO: Get rid of insecure routes
+export KUBEFLOW_URL=$(oc get routes -n istio-system secure-kubeflow -o jsonpath='http://{.spec.host}/')
+;;
+2 ) # k8s
+# Get UI address
+# see: https://istio.io/latest/docs/tasks/traffic-management/ingress/ingress-control/
+export HTTPS_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
+export KUBEFLOW_URL=https://$externalIpAddress:$HTTPS_INGRESS_PORT
+;;
+esac
+
+cat << POSTINSTALL
+Kubeflow deployed successfully.
+
+Next:
+1. Go to: $KUBEFLOW_URL
+2. If a custom certificate (e.g, istio-ingressgateway.istio-system.svc) certificate as trusted (or type "thisisunsafe" into your browser)
+3. Login using the default account:
+  - Username: user@example.com
+  - Password: 12341234
+POSTINSTALL
