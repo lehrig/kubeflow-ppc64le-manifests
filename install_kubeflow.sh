678b673a9726a224c9ffca965fbc07d40bf60f80
Marvin Gießing <marvin.giessing@gmail.com>
2023-08-30 20:38:36 +0200

    Update install_kubeflow.sh

--- install_kubeflow.sh
+++ install_kubeflow.sh
@@ -1029,7 +1029,8 @@ htpasswd -Bb $KUBEFLOW_BASE_DIR/ocp_users.htpasswd user@example.com 12341234
 oc create secret generic htpass-secret \
   --from-file=htpasswd=$KUBEFLOW_BASE_DIR/ocp_users.htpasswd \
   -n openshift-config
-oc apply -f $KUBEFLOW_KUSTOMIZE/servicemesh/htpasswd-oauth.yaml
+oc patch oauth cluster --type=merge -p '{"spec":{"identityProviders": [{"htpasswd": {"fileData": {"name": "htpass-secret"}}, "mappingMethod": "claim", "name": "Local Password","type":"HTPasswd"}]}}' --dry-run=client -o yaml
+
 
 # Get UI address
 # TODO: Get rid of insecure routes
